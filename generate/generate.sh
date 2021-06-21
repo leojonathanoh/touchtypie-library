4ea91a63894cec9d342c9131ccccf7b34828f716
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-06-21 15:41:06 +0000

    Docs (index.md): Add a touchtypie practice link alongside each URL

--- generate/generate.sh
+++ generate/generate.sh
@@ -8,12 +8,12 @@ BASE_DIR=$( git rev-parse --show-toplevel )
 REPOSITORY_NAME=$( basename "$BASE_DIR" )
 NAMESPACE=$( basename $( dirname "$BASE_DIR" ) )
 
-MD_TABLE_TITLE='| Libraries | '
-MD_TABLE_ALIGNER='|---|'
+MD_TABLE_TITLE='| Libraries | Practice | '
+MD_TABLE_ALIGNER='|---|---|'
 MD_TABLE_CONTENT=$(
     find libraries/ -type f | sort -h | while read -r l; do
         link="https://$NAMESPACE.github.io/$REPOSITORY_NAME/$l"
-        echo "| [$link]($link) |";
+        echo "| [$link]($link) | [Play now](https://play.touchtypie.com?book_library_ids=$link) |";
     done
 )
 
@@ -22,31 +22,33 @@ $MD_TABLE_TITLE
 $MD_TABLE_ALIGNER
 $MD_TABLE_CONTENT
 
+
 EOF
 
-MD_TABLE_TITLE='| Collections | '
-MD_TABLE_ALIGNER='|---|'
+MD_TABLE_TITLE='| Collections| Practice | '
+MD_TABLE_ALIGNER='|---|---|'
 MD_TABLE_CONTENT=$(
     find collections/ -type f | sort -h | while read -r l; do
         link="https://$NAMESPACE.github.io/$REPOSITORY_NAME/$l"
-        echo "| [$link]($link) |";
+        echo "| [$link]($link) | [Play now](https://play.touchtypie.com?book_collection_ids=$link) |";
     done
-)
+)r
 
 cat - >> index.md <<EOF
 $MD_TABLE_TITLE
 $MD_TABLE_ALIGNER
 $MD_TABLE_CONTENT
+
 
 EOF
 
 
-MD_TABLE_TITLE='| Books | '
-MD_TABLE_ALIGNER='|---|'
+MD_TABLE_TITLE='| Books | Practice | '
+MD_TABLE_ALIGNER='|---|---|'
 MD_TABLE_CONTENT=$(
     find books/ -type f | sort -h | while read -r l; do
         link="https://$NAMESPACE.github.io/$REPOSITORY_NAME/$l"
-        echo "| [$link]($link) |";
+        echo "| [$link]($link) | [Play now](https://play.touchtypie.com?playmode=repeatone&book_ids=$link) |";
     done
 )
 
@@ -54,5 +56,6 @@ cat - >> index.md <<EOF
 $MD_TABLE_TITLE
 $MD_TABLE_ALIGNER
 $MD_TABLE_CONTENT
+
 
 EOF
